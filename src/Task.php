f37c3c012a7879374b02fdd27ed16fefb2aa2018
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-16 11:45:48 -0700

    Instantiate  property to getAll method in Task.php

--- src/Task.php
+++ src/Task.php
@@ -45,7 +45,8 @@
             $tasks = array();
             foreach($returned_tasks as $task) {
                 $description = $task['description'];
-                $new_task = new Task($description, $id = null);
+                $id = $task['id'];
+                $new_task = new Task($description, $id);
                 array_push($tasks, $new_task);
             }
             return $tasks;
