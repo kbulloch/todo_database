992c1393219d246f6c90aacb6f869bd00e1541b0
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-16 11:14:31 -0700

    Add TaskTest.php with test_save function only

---
+++ tests/TaskTest.php
@@ -0,0 +1,30 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Task.php";
+
+    $DB = new PDO('pgsql:host=localhost;dbname=to_do_test');
+
+
+    class TaskTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_save()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $test_task = new Task($description);
+
+            //Act
+            $test_task->save();
+
+            //Assert
+            $result = Task::getAll();
+            $this->assertEquals($test_task, $result[0]);
+        }
+    }
+?>
