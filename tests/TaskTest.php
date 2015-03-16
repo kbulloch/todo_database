d251d1ed14388a8b35889dea1ee96d20c27c0755
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-16 11:20:16 -0700

    Add test_getAll to TaskTest.php

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -26,5 +26,22 @@
             $result = Task::getAll();
             $this->assertEquals($test_task, $result[0]);
         }
+
+        function test_getAll()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_Task = new Task($description);
+            $test_Task->save();
+            $test_Task2 = new Task($description2);
+            $test_Task2->save();
+
+            //Act
+            $result = Task::getAll();
+
+            //Assert
+            $this->assertEquals([$test_Task, $test_Task2], $result);
+        }
     }
 ?>
