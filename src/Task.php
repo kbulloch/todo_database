79f12ef36eddc76b70b8b2658024159ee09bfd2d
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-16 11:17:04 -0700

    Update save function to use db

--- src/Task.php
+++ src/Task.php
@@ -20,7 +20,7 @@
 
         function save()
         {
-            array_push($_SESSION['list_of_tasks'], $this);
+            $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
         }
 
         static function getAll()
