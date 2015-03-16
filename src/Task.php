588f216d43357cd9840271770b38d41b06e5df18
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-16 11:53:13 -0700

    Add find method to Task.php

--- src/Task.php
+++ src/Task.php
@@ -56,5 +56,18 @@
         {
             $GLOBALS['DB']->exec("DELETE FROM tasks *;");
         }
+
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+            foreach($tasks as $task) {
+                $task_id = $task->getID();
+                if ($task_id == $search_id) {
+                    $found_task = $task;
+                }
+            }
+            return $found_task;
+        }
     }
 ?>
