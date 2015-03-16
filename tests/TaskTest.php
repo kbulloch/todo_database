98d8d98942a19b4b8e9d29ba120b28b587b631ca
Kyle Bulloch & Jill Kuchman <student@epicodus.com>
2015-03-16 11:23:32 -0700

    Add test_deleteAll to TaskTest.php

--- tests/TaskTest.php
+++ tests/TaskTest.php
@@ -43,5 +43,23 @@
             //Assert
             $this->assertEquals([$test_Task, $test_Task2], $result);
         }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_Task = new Task($description);
+            $test_Task->save();
+            $test_Task2 = new Task($description2);
+            $test_Task2->save();
+
+            //Act
+            Task::deleteAll();
+
+            //Assert
+            $result = Task::getAll();
+            $this->assertEquals([], $result);
+        }
     }
 ?>
